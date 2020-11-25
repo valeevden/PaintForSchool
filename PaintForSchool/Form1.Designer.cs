efc66e93896f5bb826ec1f1ce97d8131ab797911
zeta0ne <space1signs@gmail.com>
2020-11-25 16:08:49 +0300

    Line works, no class yet

--- PaintForSchool/Form1.Designer.cs
+++ PaintForSchool/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace PaintForSchool
+﻿namespace PaintForSchool
 {
     partial class Form1
     {
@@ -29,13 +28,41 @@ namespace PaintForSchool
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1230, 816);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1263, 863);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- PaintForSchool/Form1.cs
+++ PaintForSchool/Form1.cs
@@ -12,9 +12,70 @@ namespace PaintForSchool
 {
     public partial class Form1 : Form
     {
+        Bitmap _mainBitmap; //Объект Bitmap используется для работы с изображениями, определяемыми данными пикселей
+        Bitmap _tmpBitmap;
+        Graphics _graphics; //класс с методами для рисования
+        Image _image; //
+        Pen _pen; //класс с инструментами для рисования
+        PointF _pt1;
+        PointF _pt2;
+        bool _mouseUp;
+        bool _mouseDown;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _mainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            _graphics = Graphics.FromImage(_mainBitmap); //в экземпляр класса графикс кладётся ссылка на битмап
+                                                         //теперь все действия которые делаются с помощью Графикс передаются в битмап
+            _pen = new Pen(Color.Red, 6);
+            _mouseDown = false;
+            //pictureBox1.Image = _mainBitmap; //в пикчербокс передаётся битмап, потому что ПБ ест только изображения
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseDown = true;
+            _pt1 = e.Location;
+            _mouseUp = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mouseDown == true && _mouseUp == false)
+            {
+                _tmpBitmap = new Bitmap(_mainBitmap); //конструктор временного битмапа на основе основного битмапа создаёт копию
+                _graphics = Graphics.FromImage(_tmpBitmap); //графикс рисует на временном битмапе
+                _pt2 = e.Location;
+                _graphics.DrawLine(_pen, _pt1, _pt2);
+                pictureBox1.Image = _tmpBitmap;
+                GC.Collect();
+            }
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _mouseUp = true;
+            _mouseDown = false;
+            _pt2 = e.Location;
+            _mainBitmap = _tmpBitmap;
+            //_graphics.DrawLine(_pen, _pt1, _pt2);
+            //pictureBox1.Image = _mainBitmap;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            _graphics.Clear(Color.Transparent);
         }
     }
 }
