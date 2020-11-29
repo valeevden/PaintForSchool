5b647dc03e89effd2f52f95495f2392503fc8f0d
zeta0ne <space1signs@gmail.com>
2020-11-29 19:34:59 +0300

    Line2D done

--- PaintForSchool/Figures/Line2D.cs
+++ PaintForSchool/Figures/Line2D.cs
@@ -4,25 +4,34 @@ using System.Linq;
 using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
+using PaintForSchool.Painter;
 
 namespace PaintForSchool.Figures
 {
-    public class Line2D// : IFigure
+    public class Line2D : IFigure
     {
-        public string fType { get; }
+        public Point startPoint { get; set; }
+        public Point secondPoint { get; set; }
+        //string fType { get; }
 
-        //public int anglesNumber { get; set; }
+        public IPainter Painter { get; }
 
         public Line2D()
         {
-            fType = "";
+            Painter = new PolygonIPainter();
         }
-        public Point[] GetPoints(Point startPoint, Point endPoint, int anglesNumber)
+
+        public Point[] GetPoints()
         {
             Point[] points = new Point[2];
             points[0] = startPoint;
-            points[1] = endPoint;
+            points[1] = secondPoint;
             return points;
+        }
+
+        public void Set(Point pointFromForm)
+        {
+            startPoint = pointFromForm;
         }
     }
 }
