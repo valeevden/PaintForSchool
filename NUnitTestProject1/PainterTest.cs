10e65606dd1d501b61ca0034fd66983d936a8c63
Alina <leesha_@mail.ru>
2020-12-11 00:40:44 +0300

    hop

--- NUnitTestProject1/PainterTest.cs
+++ NUnitTestProject1/PainterTest.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PaintForSchool.Fabrics;
+using PaintForSchool.Figures;
+using NUnit.Framework;
+using System.Drawing;
+using System.Collections;
 
 namespace NUnitTestProject1
 {
     class PainterTest
     {
+        [Test]
+        public void IsFCreateFigureTest()
+        {
+
+        }
+
     }
 }
