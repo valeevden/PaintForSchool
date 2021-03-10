3da04f01c240b4be26b3fb57129596b84837ebf9
Denis Valeev <valeevden@gmail.com>
2021-03-10 23:58:58 +0300

    123

--- PaintForSchool/Figures/Rectangle.cs
+++ PaintForSchool/Figures/Rectangle.cs
@@ -13,7 +13,7 @@ namespace PaintForSchool.Figures
 {
     public class RectangleFigure : IFigure
     {
-
+        public Point1 startPoint { get; set; }
         public Point startPoint { get; set; }
         public Point prefPoint { get; set }
         public Point secondPoint { get; set; }
